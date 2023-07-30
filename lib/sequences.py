cecd4f7b7a2e6a30bd6ad6b72e9961787eae951f
Harpreet Singh <hurpreet@icloud.com>
2023-07-30 00:02:05 -0400

    Lab Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    first_number = 0
+    second_number = 1
+    fib = []
+    for i in range(length):
+        fib.append(first_number)
+        first_number, second_number = second_number, first_number + second_number
+
+    print(fib)
+
+
+print_fibonacci(10)
